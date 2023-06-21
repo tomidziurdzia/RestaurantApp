ec5a2657d1eb2fb894a21dd50b97c685cf4b7b0e
Tomas Dziurdzia <tomidziurdzia@gmail.com>
2023-06-22 09:40:35 +1000

    Editar Platillo y guiardar en la DB

--- src/components/ui/Platillo.jsx
+++ src/components/ui/Platillo.jsx
@@ -1,14 +1,46 @@
-import React from "react";
+import { useContext, useRef } from "react";
+import { FirebaseContext } from "../../firebase";
 
 const Platillo = ({ platillo }) => {
-  const { nombre, imagen, categoria, existencia, precio, descripcion } =
+  // Existencia Ref
+  const existenciaRef = useRef(platillo.existencia);
+  const { firebase } = useContext(FirebaseContext);
+
+  const { id, nombre, imagen, categoria, existencia, precio, descripcion } =
     platillo;
+
+  const actualizarDisponibilidad = () => {
+    const existencia = existenciaRef.current.value === "true";
+
+    try {
+      firebase.db.collection("productos").doc(id).update({
+        existencia,
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <div className="w-full px-3 mb-4">
       <div className="p-5 shadow-md bg-white">
         <div className="lg:flex">
           <div className="lg:w-5/12 xl:w-3/12">
             <img src={imagen} alt="imagen" />
+            <div className="sm:flex sm:-mx0-2">
+              <label htmlFor="" className="bloc mt-5 sm:w-1/2">
+                <span className="block text-gray-800 mb-2">Existencia</span>
+                <select
+                  value={existencia}
+                  ref={existenciaRef}
+                  onChange={() => actualizarDisponibilidad()}
+                  className="shadow appearance-none border rounded w-full py-2 px-3 pl-2 leading-tight focus:outline-none bg-white"
+                >
+                  <option value="true">Disponible</option>
+                  <option value="false">No disponible</option>
+                </select>
+              </label>
+            </div>
           </div>
           <div className="lg:w-7/12 xl:w-9/12 pl-5">
             <p className="font-bold text-2xl text-yellow-600 mb-4">{nombre}</p>
